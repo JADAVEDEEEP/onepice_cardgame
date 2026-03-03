30b37760dff41bc61a115aff9638bc8a7ba76ac5
JADAVEDEEEP <jadavdeep560@gmail.com>
2026-03-03 13:13:52 +0530

    day2_commit

--- config/configdb.js
+++ config/configdb.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const HARDCODED_MONGO_URI =
+  "mongodb+srv://jadavdeep560_db_user:bSiiJsKaASjZMf9v@mediware-cluster.xnwbxvv.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGO_URI || process.env.MONGODB_URI;
+    const mongoUri = HARDCODED_MONGO_URI;
     if (!mongoUri) {
-      throw new Error("MONGO_URI or MONGODB_URI is not configured");
+      throw new Error("HARDCODED_MONGO_URI is not configured");
     }
 
     await mongoose.connect(mongoUri);
